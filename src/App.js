8f964e9897b5a97fd82b6e9fa37001144112e8c0
Veeramanikandan <maniarjun161001@gmail.com>
2024-03-26 18:34:09 +0530

    sign code commit.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import SignatureCanvas from 'react-signature-canvas'
+import { useState } from 'react';
 
 function App() {
+  const [sign,setSign]= useState();
+  const handleclear= ()=>{
+    sign.clear();
+  }
   return (
     <div className="App">
       <header className="App-header">
         <p>Hi Veeramanikandan A</p>
+        <div style={{border:"2px solid black",width: 500, height: 200}}>
+          <SignatureCanvas
+          ref={data=>setSign(data)}
+          canvasProps={{width: 500, height: 200, className: 'sigCanvas'}}
+          />
+        </div>
+        <button onClick={handleclear}>Clear</button>
+        {/* <button onClick={handlesave}>Save</button> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
